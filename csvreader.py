bf3a05cc0cdc3d2261227fcb4803e812f0688348
bharadwajtetali <bharadwajtetali@gmail.com>
2018-03-12 14:46:15 -0400

    Introduced panda

--- csvreader.py
+++ csvreader.py
@@ -1,34 +1,11 @@
 import csv
 import matplotlib.pyplot as plt
+import pandas as pd
 
-fig = plt.figure(figsize=(6, 4))
-sub1 = plt.subplot(2, 2, 1)
-
-x=[]
-y=[]
-
-with open('androaccel.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        x.append(float(row[0]))
-        y.append(float(row[1]))
+df = pd.read_csv("androaccel.csv",sep=",")
+df.set_index('Time', inplace=True)
 
 
-sub2 = plt.subplot(2, 2, 2)
 
-x1=[]
-y1=[]
-
-with open('androaccel.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        x1.append(float(row[2]))
-        y1.append(float(row[2]))
-
-
-plt.plot(x,y, label='Loaded from file!')
-plt.xlabel('Accelerometer X axis')
-plt.ylabel('Accelerometer Y axis')
-plt.title('Interesting Graph')
-plt.legend()
+df.plot()
 plt.show()
